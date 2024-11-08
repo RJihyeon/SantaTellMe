db6b3f1c2846bfe6c02b76a461a937b9133dafc1
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-11-08 15:00:28 +0900

    add config module

---
+++ backend/core/Config.py
@@ -0,0 +1,10 @@
+import dotenv
+
+env = dotenv.dotenv_values("../.env")
+
+class Config:
+    class Kakako:
+        ACCESS_KEY = env["KAKAO_CLIENT_KEY"]
+        REDIRECT_URI = env["REDIRECT_URI"]
+    class JWT:
+        SECRET_KEY = env["SECRET_KEY"]
