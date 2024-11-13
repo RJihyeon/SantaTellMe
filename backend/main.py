0d2f4d49d91c71efde6b74952da4f769bcdcb4ab
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-11-14 08:37:17 +0900

    에러 로깅하는 middleware 추가

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from api import auth_router, voice_router
+from core import catch_exceptions_middleware
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
@@ -12,5 +13,7 @@ logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 app = FastAPI()
 app.include_router(auth_router.router)
 app.include_router(voice_router.router)
+
+app.middleware("http")(catch_exceptions_middleware)
 
 uvicorn.run(app, host="0.0.0.0", port=8080)
