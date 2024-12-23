ab7316649faefaf2768b99670e1d0b8de8db1153
junhyun bang <flintintoe@gmail.com>
2024-12-23 17:27:52 +0900

    feat: Add invite page

---
+++ frontend/app/components/invite/SectionReceiveLink.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SectionReceiveLink: React.FC = () => {
+  return (
+    <section>
+      <div className="wrapper">
+        <div className="h-72 bg-red-200 rounded-lg p-4 mx-auto">
+          <h3>Paste link here:</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionReceiveLink;
