b1de318cdf1c40030e158e88fdbbdf7bdf0c9ce4
junhyun bang <flintintoe@gmail.com>
2024-12-24 00:57:38 +0900

    fix: Cannot login from docker build deployment

--- frontend/app/api/login.ts
+++ frontend/app/api/login.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("API_BASE_URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
+console.log("NEXT_PUBLIC_API_EXTERNAL_BASE_URL:", process.env.NEXT_PUBLIC_API_EXTERNAL_BASE_URL);
 
 const login = async () => {
-  window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/login`;
+  window.location.href = `${process.env.NEXT_PUBLIC_API_EXTERNAL_BASE_URL}/login`;
 };
 
 export default login;
