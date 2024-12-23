a9cc1b1a12dd9c06fae2c362cf33e1b8ab5317f7
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-12-24 07:06:36 +0900

    foo

--- backend/app/rvc/request.py
+++ backend/app/rvc/request.py
@@ -49,7 +49,6 @@ def rvc_infer_request(input_wav: bytes) -> bytes:
         raise HTTPException(status_code=500, detail={"detail": error_msg})
 
     try:
-        print(resp.content)
         resp_body = json.loads(resp.content)
         output_wav = base64.b64decode(resp_body['wav_b64'])
     except Exception as e:
